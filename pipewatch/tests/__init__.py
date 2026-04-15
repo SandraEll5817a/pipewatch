06c06218fccc3641acaa3a5a4fa37b5da99578a7
Sandra Ellis <SandraEll5817a@users.noreply.github.com>
2026-04-15 22:57:39 +0800

    Add get_test_data_path helper to tests package

--- pipewatch/tests/__init__.py
+++ pipewatch/tests/__init__.py
@@ -1,1 +1,16 @@
 # pipewatch tests package
+
+import os
+import sys
+
+
+def get_test_data_path(filename):
+    """Return the absolute path to a test data file.
+
+    Args:
+        filename: The name of the file within the test data directory.
+
+    Returns:
+        The absolute path to the specified test data file.
+    """
+    return os.path.join(os.path.dirname(__file__), "data", filename)
